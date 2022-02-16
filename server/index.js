1e1687644ac4665cba43a475e186ac494e2f9785
Hung Le <hunglek16@gmail.com>
2022-02-16 16:37:43 +0700

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./routes/auth')
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://hungk16:1234@cluster0.0lgg0.mongodb.net/mern-learnt?retryWrites=true&w=majority`, {
+    })
+      console.log('MongoDB connected')
+  } catch (error){
+      console.log(error.message)
+      process.exit(1)
+  }
+}
+
+connectDB()
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello word'))
+app.use('/auth', authRouter)
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
